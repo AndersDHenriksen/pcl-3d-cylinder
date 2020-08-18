1a674cbd481edffa5aed590d6d057b913b49acdc
Anders Dahl Henriksen <ahe@proinvent.dk>
2020-08-18 08:35:38 +0200

    -Added more attempts

--- VariousScripts/bad_perp.py
+++ VariousScripts/bad_perp.py
@@ -4,7 +4,7 @@ import open3d as o3d
 import os
 
 from open3d_helper import perp, cloud_to_nparray, nparray_to_cloud, visualize_cloud, icp_multiple, vectors_to_transfor, \
-    get_rotation_matrix, normalize, get_normals, plane_fitting, cylinder_fitting, draw_registration_result
+    get_rotation_matrix, normalize, get_normals, plane_fitting, cylinder_fitting, draw_registration_result, transform_cloud
 
 diameter = 330
 height = 76
@@ -25,7 +25,7 @@ print(f"Top perp is: {perp(normal, cyl_axis, diameter):.2f} mm")
 print(f"Top CutSt is: {perp(normal, cyl_axis, height):.2f} mm")
 _ = 'bp'
 
-# New attempt align with icp because of tops - Works better but not sure why - Also seems incorrect to do
+# New attempt align with icp because of tops - Works better but not sure why - Also seems incorrect to do as top and side is not aligned perfectly
 combined = [top[i] + side[i] for i in range(4)]
 total_cloud = icp_multiple(combined[0], combined[1:])
 # total_cloud = icp_multiple(side[0], side[1:])  # Not working
@@ -69,9 +69,9 @@ print(f"Combined perp: {perp(combined_normal, combined_axis, diameter):.2f} mm")
 # print(perp_new)
 
 # next attempt - outside lib
-from cylinder_analysis_o3d import fit
-cyl_all = cloud_to_nparray(side[0] + side[1] + side[2] + side[3])
-w_fit, C_fit, r_fit, fit_err = fit(cyl_all)
+# from cylinder_analysis_o3d import fit
+# cyl_all = cloud_to_nparray(side[0] + side[1] + side[2] + side[3])
+# w_fit, C_fit, r_fit, fit_err = fit(cyl_all)
 
 # next attempt, fit lines to 100 side segments
 cyl_all = cloud_to_nparray(side[0] + side[1] + side[2] + side[3])
@@ -92,3 +92,82 @@ cyl_axis2 = normalize(np.mean(angle_axis, axis=0))
 
 print(f"Top perp is: {perp(normal, cyl_axis2, diameter):.2f} mm")
 print(f"Top CutSt is: {perp(normal, cyl_axis2, height):.2f} mm")
+
+# next attempt - correct side slope by icp to top surface first - side scanner must measure top surface incorrectly?
+new_side, new_side2 = 4 * [None], 4 * [None]
+for i in range(4):
+    # new_side[i] = icp_multiple(top[i], [side[i]])
+    max_iterations = 100
+    threshold = 4
+    trans_init = np.eye(4)
+    reg_p2p = o3d.registration.registration_icp(side[i], top[i], threshold, trans_init,
+                                                o3d.registration.TransformationEstimationPointToPoint(),
+                                                o3d.registration.ICPConvergenceCriteria(max_iteration=max_iterations))
+    new_side[i] = transform_cloud(side[i], reg_p2p.transformation)
+    reg_p2p = o3d.registration.registration_icp(new_side[i], top[0], threshold, trans_init,
+                                                o3d.registration.TransformationEstimationPointToPoint(),
+                                                o3d.registration.ICPConvergenceCriteria(max_iteration=max_iterations))
+    new_side2[i] = transform_cloud(new_side[i], reg_p2p.transformation)
+    # check that it worked
+    print(perp(plane_fitting(top[i], diameter), plane_fitting(new_side[i], diameter), diameter))
+cyl_axis = cylinder_fitting(new_side[0] + new_side[1] + new_side[2] + new_side[3], height)
+print(f"Top perp is: {perp(normal, cyl_axis, diameter):.2f} mm")
+combined_cloud = icp_multiple(new_side[0], new_side[1:])
+cyl_axis = cylinder_fitting(combined_cloud, height)
+normal0 = plane_fitting(top[0], diameter)
+print(f"Top perp is: {perp(normal0, cyl_axis, diameter):.2f} mm")
+
+# next attempt break each section into angles, works somewhat
+angle_delimeter = np.linspace(-np.pi, np.pi, 101)
+axes = np.zeros((4, angle_delimeter.size - 1, 3))
+for s in range(4):
+    print(f"S{s}")
+    scan = cloud_to_nparray(side[s])
+    good_idx = (scan[:, 2] < height - 10) & (scan[:, 2] > 10)
+    side_filt = scan[good_idx, :]
+    side_angles = np.arctan2(side_filt[:, 1], side_filt[:, 0])
+    for i in range(angle_delimeter.size - 1):
+        angle_lower = angle_delimeter[i]
+        angle_higher = angle_delimeter[i + 1]
+        angle_idx = (side_angles > angle_lower) & (side_angles < angle_higher)
+        if angle_idx.sum() < 500:
+            continue
+        cyl_reduced = side_filt[angle_idx, :]
+        U, S, VT = np.linalg.svd(cyl_reduced - cyl_reduced.mean(axis=0))
+        angle_axis = VT[0, :] if VT[0, 2] > 0 else -VT[0, :]
+        axes[s, i, :] = angle_axis
+        # print(f"Top perp is: {perp([0, 0, 1], angle_axis, diameter):.2f} mm")
+perp(normal, normalize(np.array([normalize(axes[s, axes[s,:,:].any(axis=-1), :].mean(axis=0)) for s in range(4)]).mean(axis=0)), diameter)
+
+# next attempt, do 4 cylinder fittings on best piece of cloud
+cyl_axes = 4 * [None]
+for i in range(4):
+    scan = cloud_to_nparray(side[i])
+    good_idx = (scan[:, 2] < height - 10) & (scan[:, 2] > 10)
+    side_filt = scan[good_idx, :]
+    side_angles = np.arctan2(side_filt[:, 1], side_filt[:, 0])
+    mean_angle = np.arctan2(side_filt[:, 1].mean(), side_filt[:, 0].mean()) #side_angles.mean()
+    if min(np.abs(mean_angle - np.pi), np.abs(mean_angle + np.pi)) < np.pi/8:
+        offset = np.pi if mean_angle < 0 else -np.pi
+        mean_angle += offset
+        side_angles += offset
+        side_angles[side_angles < -np.pi] += 2 * np.pi
+        side_angles[side_angles > np.pi] -= 2 * np.pi
+    good_idx2 = np.abs(side_angles - mean_angle) < np.pi/4
+    side_filt2 = side_filt[good_idx2, :]
+    side_angles2 = side_angles[good_idx2]
+    # print(side_filt2.size)
+    # cyl_axes[i] = cylinder_fitting(nparray_to_cloud(side_filt2), height)  # Code below is fitting with angle delimeter method
+    angle_delimeter = np.linspace(-np.pi/4, np.pi/4, 21) + mean_angle
+    angle_axes = []
+    for j in range(angle_delimeter.size - 1):
+        angle_lower = angle_delimeter[j]
+        angle_higher = angle_delimeter[j + 1]
+        angle_idx = (side_angles2 > angle_lower) & (side_angles2 < angle_higher)
+        cyl_reduced = side_filt2[angle_idx, :]
+        U, S, VT = np.linalg.svd(cyl_reduced - cyl_reduced.mean(axis=0))
+        angle_axes.append(VT[0, :] if VT[0, 2] > 0 else -VT[0, :])
+    cyl_axes[i] = normalize(np.array(angle_axes).mean(axis=0))
+
+cyl_axis = normalize(np.array(cyl_axes).mean(axis=0))
+print(f"Top perp is: {perp(normal, cyl_axis, diameter):.2f} mm")
